6cb4c46c0f871767f82366a354841dee776fbc53
Dylan Ranger <dylan.ranger.dev@gmail.com>
2024-11-26 21:03:20 +0200

    Add seeding for db

---
+++ snap-sanatise-be/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO words (word) VALUES ('brown');
+INSERT INTO words (word) VALUES ('purple');
+INSERT INTO words (word) VALUES ('violet');
+INSERT INTO words (word) VALUES ('orange');
+INSERT INTO words (word) VALUES ('yellow');
+INSERT INTO words (word) VALUES ('green');
+INSERT INTO words (word) VALUES ('red');
+INSERT INTO words (word) VALUES ('blue');
