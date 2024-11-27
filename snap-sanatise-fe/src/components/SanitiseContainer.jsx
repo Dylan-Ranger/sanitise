f2c339344d5831a9dd7ab850917e281c1d129f96
Dylan Ranger <dylan.ranger.dev@gmail.com>
2024-11-27 08:28:48 +0200

    Create basic sanitise container

--- snap-sanatise-fe/src/components/SanitiseContainer.jsx
+++ snap-sanatise-fe/src/components/SanitiseContainer.jsx
@@ -1,8 +1,18 @@
 export default function SanitiseContainer() {
+  // TODO: Loader
+  // TODO: Output
   return (
     <div>
-      <input type="text" placeholder="Add the text you would like to sanitise"></input>
-      <button>Sanatise Text</button>
+      <form className="flex flex-col">
+        <textarea
+          className="rounded-md border p-4"
+          placeholder="Add the text you would like to sanitise"
+          rows="8"
+        ></textarea>
+        <button className="text-white bg-gray py-2 px-4 w-[150px] rounded-xl">
+          Sanitise Text
+        </button>
+      </form>
     </div>
   )
 }
