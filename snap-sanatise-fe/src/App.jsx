705a811c0b0c198f97792ef853ab9b6492bda278
Dylan Ranger <dylan.ranger.dev@gmail.com>
2024-11-27 08:29:26 +0200

    Set up initial UI

--- snap-sanatise-fe/src/App.jsx
+++ snap-sanatise-fe/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import Words from "./components/Words.jsx";
+import SanitiseContainer from "./components/SanitiseContainer.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="h-svh py-8 lg:w-3/4">
+      <h1 className="text-4xl font-extrabold tex-black">Word Sanitiser</h1>
       <Words />
-    </>
+      <SanitiseContainer />
+    </div>
   )
 }
 
