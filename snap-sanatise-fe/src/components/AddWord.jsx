f83861e81f1eecf80ee8339556d11f623f542053
Dylan Ranger <dylan.ranger.dev@gmail.com>
2024-11-28 05:56:23 +0200

    Add edit and delete functionality for words

--- snap-sanatise-fe/src/components/AddWord.jsx
+++ snap-sanatise-fe/src/components/AddWord.jsx
@@ -2,20 +2,24 @@ import {createPortal} from "react-dom";
 import {forwardRef, useImperativeHandle, useRef, useState} from "react";
 
 const AddWord = forwardRef(function AddWord(
-  {onUpdate}, ref
+  {onUpdate, onDelete, editWord= null}, ref
 ) {
+  const isEdit = editWord !== null
   const dialog = useRef()
-  const [word, setWord] = useState('');
-  const [errorMessage, setErrorMessage] = useState()
+  const [word, setWord] = useState(isEdit ? editWord.word : '');
+  const [errorMessage, setErrorMessage] = useState('')
 
   useImperativeHandle(ref, () => {
     return {
       open() {
         dialog.current.showModal()
+      },
+      close() {
+        setWord('')
+        dialog.current.close()
       }
     }
   })
-
   function handleChange(event) {
     setWord(event.target.value)
   }
@@ -24,14 +28,20 @@ const AddWord = forwardRef(function AddWord(
     setErrorMessage(null)
 
     try {
-      const response = await fetch('http://localhost:8080/word', {
-        method: 'POST',
+      const url = isEdit ? `http://localhost:8080/word/${editWord.id}` : 'http://localhost:8080/word'
+      const body = isEdit ? {
+        word : word,
+        id: editWord.id
+      } : {
+        word
+      }
+
+      const response = await fetch(url, {
+        method: isEdit ? 'PUT' : 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-          word
-        }),
+        body: JSON.stringify(body),
       });
 
       if (!response.ok) {
@@ -42,13 +52,25 @@ const AddWord = forwardRef(function AddWord(
       const result = await response.json()
       onUpdate(result.word)
       setWord(null)
-      handleCloseModal()
+      dialog.current.close()
     } catch (error) {
       console.error('Error:', error);
     }
   }
 
-  function handleCloseModal() {
+  async function handleDeleteWord(event) {
+    event.preventDefault()
+    const response = await fetch(`http://localhost:8080/word/${editWord.id}` , {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    if (!response.ok) {
+      setErrorMessage("Could not delete the word!")
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    onDelete(editWord)
     dialog.current.close()
   }
 
@@ -57,11 +79,12 @@ const AddWord = forwardRef(function AddWord(
       className="bg-blue rounded-lg p-5 relative overflow-visible wordDialog"
       ref={dialog}>
       <div
-        onClick={handleCloseModal}
-        className="rounded-full bg-gray w-8 h-8 text-center leading-8 absolute top-0 right-0 -mt-2 -mr-2 font-bold text-white">
+        onClick={() => dialog.current.close()}
+        className="rounded-full bg-gray w-8 h-8 text-center leading-8 cursor-pointer
+                   absolute top-0 right-0 -mt-2 -mr-2 font-bold text-white">
         X
       </div>
-      <h2 className="text-2xl font-bold mb-6">Add a new word</h2>
+      <h2 className="text-2xl font-bold mb-6">{isEdit ? "Edit word" : "Add New Word"}</h2>
       <form
         className="flex flex-col"
         method="dialog">
@@ -74,12 +97,22 @@ const AddWord = forwardRef(function AddWord(
         {errorMessage &&
           <p className="text-sm text-red-700">{errorMessage}</p>
         }
-        <button
-          className="text-white bg-gray py-2 px-4 w-[150px] rounded-xl mt-4"
-          onClick={handleCreateWord}
-          disabled={!word}>
-          Submit
-        </button>
+        <div className="flex justify-between content-center mt-4">
+          <button
+            className="text-white bg-gray py-2 px-4 w-[150px] rounded-xl"
+            onClick={handleCreateWord}
+            disabled={!word}>
+            {isEdit ? "Update" : "Create"}
+          </button>
+          {isEdit &&
+            <img
+              src={"/trash.svg"}
+              className="cursor-pointer"
+              height="25"
+              width="25"
+              onClick={handleDeleteWord}
+              alt="trashcan"/>}
+        </div>
       </form>
     </dialog>,
     document.getElementById("modal")
