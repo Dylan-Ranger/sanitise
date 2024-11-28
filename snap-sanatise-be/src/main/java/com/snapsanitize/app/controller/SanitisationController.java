0e28e021515a828a1773db49cdef804412efd03a
Dylan Ranger <dylan.ranger.dev@gmail.com>
2024-11-28 05:57:41 +0200

    Create Sanitisation end point.

---
+++ snap-sanatise-be/src/main/java/com/snapsanitize/app/controller/SanitisationController.java
@@ -0,0 +1,34 @@
+package com.snapsanitize.app.controller;
+
+import com.snapsanitize.app.model.Word;
+import com.snapsanitize.app.service.SanitisationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/sanitise")
+public class SanitisationController {
+    private SanitisationService service;
+
+    public SanitisationController(SanitisationService service) {
+        this.service = service;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> addWord(@RequestBody String textToSanitise) {
+        String result = service.sanitise(textToSanitise);
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "String sanitised successfully");
+        response.put("result", result);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
--- snap-sanatise-be/src/main/java/com/snapsanitize/app/service/SanitisationService.java
+++ snap-sanatise-be/src/main/java/com/snapsanitize/app/service/SanitisationService.java
@@ -1,4 +1,34 @@
 package com.snapsanitize.app.service;
 
+import com.snapsanitize.app.model.Word;
+import com.snapsanitize.app.repository.WordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.regex.Pattern;
+
+@Service
 public class SanitisationService {
+    private final WordRepository repository;
+
+    @Autowired
+    public SanitisationService(WordRepository repository) {
+        this.repository = repository;
+    }
+
+    public String sanitise(String userInput) {
+        List<Word> words = repository.findAll();
+
+        String sanitisedString = userInput;
+        for (Word word : words) {
+            String token= "*".repeat(word.getWord().length());
+            String regex = "(?i)\\b\\Q" + word.getWord() + "\\E\\b(?=[.,?!\\s]*|$)(?!\\s*-|')";
+            sanitisedString = sanitisedString.replaceAll(regex, token);
+        }
+        return sanitisedString;
+    }
+
+    private boolean stringContainsSpecialCharacters(String string) {
+        return Pattern.compile(".*[^a-zA-Z0-9].*").matcher(string).matches();
+    }
 }
