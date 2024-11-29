50dbff589321186fa16e09f3de35c5d9950d36bd
Dylan Ranger <dylan.ranger.dev@gmail.com>
2024-11-29 10:51:43 +0200

    Remove comment

--- snap-sanatise-be/src/main/java/com/snapsanitize/app/repository/WordRepository.java
+++ snap-sanatise-be/src/main/java/com/snapsanitize/app/repository/WordRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface WordRepository extends JpaRepository<Word, Long> {
-    Optional<Word> findByWord(String word);  // Query by the 'word' field
+    Optional<Word> findByWord(String word);
 }
